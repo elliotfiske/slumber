f882760bfa2c6a762032a727f20467d38874e752
elliotfiske <elliotfiske@gmail.com>
2015-04-29 13:56:51 -0700

    Setting up project to build cleanly across platforms

--- main/built/Debug/client.py
+++
@@ -1,8 +0,0 @@
-import requests, sys
-url = 'http://www.elliotfiske.com/slumber.py'
-
-# GET
-r = requests.get(url)
-
-# print r.text.strip()[-1]
-sys.exit(int(r.text.strip()[-1]))
--- main/built/Lab1.build/Debug/Lab1.build/StaticAnalyzer/Lab1/Lab1/normal/x86_64/util.d
+++
@@ -1,2 +0,0 @@
-dependencies: /Users/elliotfiske/slumber/lab1/mac/source/util.c \
-  /Users/elliotfiske/slumber/lab1/mac/source/util.h
