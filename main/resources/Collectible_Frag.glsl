81662e551463093c3f6c55fc75d1c2dae2109580
elliotfiske <elliotfiske@gmail.com>
2015-05-15 20:33:39 -0700

    removed debug prints

--- main/resources/Collectible_Frag.glsl
+++ main/resources/Collectible_Frag.glsl
@@ -7,5 +7,8 @@ varying vec3 vNor;
 varying vec3 vPos;
 
 void main() {
+    gl_FragColor = vec4(vNor, 1.0);
+    gl_FragColor = vec4(vPos, 1.0); // Just get rid of the warnings :O
+    
     gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
 }
