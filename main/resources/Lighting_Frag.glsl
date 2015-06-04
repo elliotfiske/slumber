2d74099fafba8c1d44df2fdf59a24061b452a710
Noah Harper <harper92@gmail.com>
2015-06-03 20:49:00 -0700

    PCSS implemented, ghost crashing on lab machines

--- main/resources/Lighting_Frag.glsl
+++ main/resources/Lighting_Frag.glsl
@@ -15,27 +15,8 @@ varying vec4 shadowClip;
 varying vec4 highlightCoords;
 varying vec3 vLight;
 
-vec2 poissonDisk[] = vec2[](
-    vec2( -0.942011224, -0.3990122112 ),
-    vec2( 0.945581209, -0.712890725 ),
-    vec2( -0.094184101, -0.92938870 ),
-    vec2( 0.34495938, 0.293877120 ),
-    vec2( -0.91588581, 0.45771432 ),
-    vec2( -0.81544232, -0.879124124 ),
-    vec2( -0.38277543, 0.2712712845 ),
-    vec2( 0.97484398, 0.751248379 ),
-    vec2( 0.44323325, -0.97511554 ),
-    vec2( 0.53742981, -0.47373420 ),
-    vec2( -0.2124912911, -0.41893023 ),
-    vec2( 0.79197514, 0.19090188 ),
-    vec2( -0.24188840, 0.997012507 ),
-    vec2( -0.81409955, 0.91437590 ),
-    vec2( 0.199841212, 0.7812413127 ),
-    vec2( 0.143831121, -0.14100790 )
-);
-
-float FindBlocker(vec2 uv, vec4 lightPos, float searchWidth);
-float EstimatePenumbra(vec2 uv, vec4 lightPos, float blocker, float lightSize);
+float searchBlocker(vec2 uv, vec4 lightPos, float searchWidth);
+float estimatePenumbra(vec2 uv, vec4 lightPos, float blocker, float lightSize);
 float PCF(vec2 uv, vec4 lightPos, float filterWidth);
 
 void main() {
@@ -65,98 +46,86 @@ void main() {
     shadowCoords = shadowCoords / shadowCoords.w;
     // Go from [-1,1] to [0,1]
     shadowCoords.xyz = 0.5 * shadowCoords.xyz + 0.5;
-    
-    float bias = 0.000001;
-    float blur = 0.0001;
-    float visibility = 1.0;
 
-    ///////////////////////////////////////////////////
-	// STEP 1: Blocker estimate
-	float blocker = FindBlocker(shadowCoords.xy, shadowCoords, 1.0);
- 
-	///////////////////////////////////////////////////
-	// STEP 2: Penumbra size estimation
-	float penumbra = EstimatePenumbra(shadowCoords.xy, shadowCoords, blocker, 0.01);
- 
-	if(penumbra > 0.01)
-		penumbra = 0.01;
- 
-	visibility = PCF(shadowCoords.xy, shadowCoords, penumbra);
- 
-	// If no blocker, return 1.0 since the point isn't in shadow
-	if(blocker == 0.0) visibility = 1.0;
+	float visibility = 1.0;
+
+	// Only calculate if the fragment is within the shadowmap's bounds
+	if(shadowCoords.w > 0.0 &&
+        shadowCoords.x > 0.0 && shadowCoords.x < 1.0 &&
+        shadowCoords.y > 0.0 && shadowCoords.y < 1.0 &&
+        shadowCoords.z > 0.0 && shadowCoords.z < 1.0) {
+		// STEP 1: Blocker estimate
+		float blocker = searchBlocker(shadowCoords.xy, shadowCoords, 1.0);
+		// STEP 2: Penumbra size estimation
+		float penumbra = estimatePenumbra(shadowCoords.xy, shadowCoords, blocker, 0.002);
+	 
+		if(penumbra > 0.01) penumbra = 0.01;
+	 
+		visibility = PCF(shadowCoords.xy, shadowCoords, penumbra);
+
+	}
 
     gl_FragColor = vec4(lAmbientColor + visibility * (lDiffuseColor + lSpecularColor), 1.0);
 }
 
-/*
- * Search for potential blockers
- */
-float FindBlocker(vec2 uv, vec4 lightPos, float searchWidth)
+// Search near the point for blockers
+float searchBlocker(vec2 uv, vec4 lightPos, float searchWidth)
 {
-	float stepSize = 2.0 * searchWidth/12.0;
+	float stepSize = 2.0 * searchWidth/16.0;
  
 	// Starting point uv coords for search
 	uv = uv - vec2(searchWidth, searchWidth);
  
 	float blockerSum = 0.0;
 	float blockerCount = 0.0;
- 
+
 	// iterate through search region and add up depth values
-    for(int i = 0; i < 12; i += 1) {
-		for(int j = 0; j < 12; j += 1) {
-			float shadMapDepth = texture2D(shadowMap, uv + vec2(i * stepSize, j * stepSize)).z;
+    for(int i = 0; i < 16; i += 1) {
+		for(int j = 0; j < 16; j += 1) {
+			float distToLight = texture2D(shadowMap, uv + vec2(i * stepSize, j * stepSize)).z;
 
 			// found a blocker
-			if(shadMapDepth < lightPos.z) {
-				blockerSum += shadMapDepth;
+			if(distToLight < lightPos.z) {
+				blockerSum += distToLight;
 				blockerCount += 1.0;
 			}
 		}
 	}
  
-	float result = blockerSum/blockerCount; // FIXME
+	float result = blockerSum/blockerCount;
 	return result;
 }
 
-/*
- * Function to estimate the shadow penumbra size.
- */
-float EstimatePenumbra(vec2 uv, vec4 lightPos, float blocker, float lightSize)
+// Estimates the size of the penumbra
+float estimatePenumbra(vec2 uv, vec4 lightPos, float blocker, float lightSize)
 { 
        // estimate penumbra using parallel planes approximation
-       float penumbra = (lightPos.z - blocker) * lightSize/blocker;
- 
-       return penumbra;
+       return (lightPos.z - blocker) * lightSize / blocker;
 }
 
-/*
- * Percentage Closer Filtering with customizable filter kernel size and sample amount.
- */
+// Percentage Closer Filtering
 float PCF(vec2 uv, vec4 lightPos, float filterWidth)
 {
-	float stepSize = 2.0 * filterWidth/12.0;
+	float stepSize = 2.0 * filterWidth/16.0;
 
-	uv = uv - vec2(filterWidth, filterWidth); // FIXME: why not (uv - filterWidth) ?
+	uv = uv - vec2(filterWidth, filterWidth);
 
 	float sum = 0.0;
 
 	// now iterate through the kernel and filter the values
-	for(int i = 0; i < 12; i += 1) // FIXME
+	for(int i = 0; i < 16; i += 1)
 	{
-		for(int j = 0; j < 12; j += 1) // FIXME
+		for(int j = 0; j < 16; j += 1)
 		{
-			// get depth at current texel of the shadow map
-			float shadMapDepth = texture2D(shadowMap, uv + vec2(i*stepSize, j*stepSize)).z;
+			float distToLight = texture2D(shadowMap, uv + vec2(i*stepSize, j*stepSize)).z;
 
 			// test if the depth in the shadow map is closer than the eye-view point
-			float shad = lightPos.z < shadMapDepth ? 1.0 : 0.0;
+			float shadowed = lightPos.z < distToLight ? 1.0 : 0.0;
 
-			// accumulate result
-			sum += shad;
+			sum += shadowed;
 		}
 	}
 
 	// return average of the samples
-	return sum/(12.0 * 12.0);
+	return sum / (16.0 * 16.0);
 }
