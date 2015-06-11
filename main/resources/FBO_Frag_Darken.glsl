afc9dfe445f4306c372d7cfab8c5fda8227726fc
Elliot Fiske <elliotfiske@gmail.com>
2015-06-10 20:32:32 -0700

    Quick fix to woozy UVs

--- main/resources/FBO_Frag_Darken.glsl
+++ main/resources/FBO_Frag_Darken.glsl
@@ -30,7 +30,7 @@ vec2 woozyUV() {
 }
 
 void main() {
-    vec2 distortedUV = woozyUV();
+    vec2 distortedUV = (intensity < 2. ? UV : woozyUV());
     
     vec4 currColor = texture2D(uTex, distortedUV);
     
--- main/source/network_setup.h
+++ main/source/network_setup.h
@@ -11,7 +11,7 @@
 
 #include <stdio.h>
 
-#define SERVER_ADDRESS "192.168.0.1"
+#define SERVER_ADDRESS "192.168.0.2"
 #define SERVER_PORT "4444"
 #define SERVER_PORT_NUM 4444
 
